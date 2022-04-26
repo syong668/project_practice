ca906b5e08818988fa240f345f41b1cf74413a5e
syong668 <vibo0973663387@gmail.com>
2022-04-26 18:12:39 +0800

    暫時先+一個login link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a href="https://syong668.github.io/project_practice/dist/index.html#/login">登入後台</a>
   </div>
 </template>
 
