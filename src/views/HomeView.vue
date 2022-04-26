482fa250a20abc5b242503bed97d1f154d0b946e
syong668 <vibo0973663387@gmail.com>
2022-04-26 19:00:18 +0800

    暫時調整為router-link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <a href="https://syong668.github.io/project_practice/dist/index.html#/login">登入後台</a>
+    <router-link to="/login">登入後台</router-link>
   </div>
 </template>
 
