4c58f6a64d78ff49a05fceffa35c609b882b6d0d
syong668 <vibo0973663387@gmail.com>
2022-05-27 01:12:07 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/project_practice/dist/'
+  publicPath: '/sira_swim/dist/'
 })
